15ed0340c804ffe128231b989801d3db0b939aa4
choyeongju <jjudy1301@naver.com>
2024-11-18 18:28:33 +0900

    :tada: [init] 초기 세팅

---
+++ src/main/java/sopt/twosome/TwosomeHeartApplication.java
@@ -0,0 +1,13 @@
+package sopt.twosome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwosomeHeartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwosomeHeartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sopt/twosome/TwosomeHeartApplicationTests.java
@@ -0,0 +1,13 @@
+package sopt.twosome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwosomeHeartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
